e068232fcaeeb2e1e57a288f81973777d0541f3a
elenyou <olenaovch@gmail.com>
2019-06-05 10:38:10 -0400

    some style fix

--- pages/post.js
+++ pages/post.js
@@ -5,7 +5,7 @@ import { withRouter } from 'next/router';
 const Post = ({ router }) => (
 
     <Layout title={router.query.title}>
-        <p>
+        <p style={{ width: '80vw' }}>
             Maecenas eget nisi nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ac semper lectus. Sed at laoreet odio. Etiam sagittis, massa in posuere sollicitudin, erat arcu luctus lacus, vel luctus magna tortor id metus. Maecenas vitae mi fringilla, sollicitudin turpis sit amet, sollicitudin lectus.
         </p>
     </Layout>
