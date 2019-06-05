81ef73672261722abedf983e320940b60f4137ca
elenyou <olenaovch@gmail.com>
2019-06-05 10:34:41 -0400

    dinamic blog and post

---
+++ pages/post.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+import { withRouter } from 'next/router';
+
+
+const Post = ({ router }) => (
+
+    <Layout title={router.query.title}>
+        <p>
+            Maecenas eget nisi nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ac semper lectus. Sed at laoreet odio. Etiam sagittis, massa in posuere sollicitudin, erat arcu luctus lacus, vel luctus magna tortor id metus. Maecenas vitae mi fringilla, sollicitudin turpis sit amet, sollicitudin lectus.
+        </p>
+    </Layout>
+);
+
+export default withRouter(Post);
